5398726cb43a7a9468fa99214d064cb81e732d2f
Jimmy Tang <blay.tenshi@gmail.com>
2020-11-21 20:07:13 +1100

    Now shows temporary todo list
    - Added TodoList component
    - Added TodoItem component
    - Tidied up README.MD

---
+++ src/Components/TodoItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="flex">
+    <div class="justify-start item-id">
+      <slot name="id"></slot>
+    </div>
+    <div>
+      <slot name="description"></slot>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item-id {
+  width: 1em;
+}
+</style>
